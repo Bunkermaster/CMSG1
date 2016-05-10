76dd53158aa265d989de757695191098eb61fb3d
Alexandre vagnair <alexandrevagnair@MacBook-Pro-de-Alexandre.local>
2016-05-10 11:22:36 +0200

    config start /no view

---
+++ controllers/PageController.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: alexandrevagnair
+ * Date: 10/05/2016
+ * Time: 10:14
+ */
+class PageController
+{
+    public function ajoutAcition(\PDO $pdo)
+    {
+        
+    }
+    public function supprimerAction(\PDO $pdo)
+    {
+
+    }
+
+    public function detailsAction(\PDO $pdo)
+    {
+
+    }
+    public function listeAction(\PDO $pdo)
+    {
+
+    }
+    public function displayAction(\PDO $pdo)
+    {
+
+    }
+
+
+}
