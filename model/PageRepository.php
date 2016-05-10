c1f81d58cd58d607ed39b21c3c87edae5dd0d6c4
Alexandre vagnair <alexandrevagnair@MacBook-Pro-de-Alexandre.local>
2016-05-10 11:58:08 +0200

    corriger part1

--- model/PageRepository.php
+++ model/PageRepository.php
@@ -6,36 +6,43 @@
  * Date: 10/05/2016
  * Time: 10:18
  */
+namespace Model;
+
 class PageRepository
 {
-    public function update(\PDO $pdo)
+    private $pdo;
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+    public function update()
     {
         $sql = "UPDATE `page` SET  WHERE 1";
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
     }
 
-    public function insert(\PDO $pdo)
+    public function insert()
     {
         $sql ="INSERT INTO `page`() VALUES ()";
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
     }
 
-    public function delete(\PDO $pdo)
+    public function delete()
     {
         $sql = "DELETE FROM `page` WHERE 1";
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
     }
 
-    public function select(\PDO $pdo)
+    public function select()
     {
         $sql = "SELECT `id`, `slug`, `h1`, `body`, `title`, `img`, `span_text`, `span_class` FROM `page` WHERE 1";
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
     }
-    public function selectAll(\PDO $pdo)
+    public function selectAll()
     {
         $sql = "SELECT `id`, `slug`, `h1`, `body`, `title`, `img`, `span_text`, `span_class` FROM `page`";
         $stmt = $pdo->prepare($sql);
