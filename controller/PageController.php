f760d2642a4b0ee4d6c4f256e22e76cd32ff5fa8
Ikimasoft <yann@ikimasoft.fr>
2016-05-27 16:59:24 +0200

    Oh non le mastergit add .git add .

--- controller/PageController.php
+++ controller/PageController.php
@@ -23,71 +23,79 @@ class PageController
      *
      */
     public function ajoutAction()
-	{
-	}
+    {
+        if(count($_POST) === 0){
+            // affiche la form
+        } else {
+            // traite la form
+        }
+    }
 
     /**
      *
      */
     public function supprimerAction()
-	{
-	}
+    {
+    }
 
     /**
      *
      */
     public function modifierAction()
-	{
-	}
+    {
+    }
 
     /**
      *
      */
     public function detailsAction()
-	{
-	}
+    {
+    }
 
     /**
      *
      */
     public function listeAction()
-	{
-	}
+    {
+
+    }
 
     /**
      *
      */
     public function displayAction()
-	{
-		// definition d'un slug par defaut (en cas d'appel sans parametre dans l'url)
-		$slug = 'teletubbies';
-		// recuperation du slug du parametre d'url si present
-		if(isset($_GET['p'])){
-			$slug = $_GET['p'];
-		}
-		// en PHP 7
-		// $slug = $_GET['p'] ?? $_POST['p'] ?? 'teletubbies';
-		// recuperation les donnees de la page qui correspond au slug
-		$page = $this->repository->getBySlug($slug);
-		// si il n'y a pas de donnees, erreur 404
-		if($page === false){
-			// 404
-			include "view/404.php";
-			return;
-		}
-		// je dois avoir la nav initialisee pour que la vue la montre
-		$nav = $this->genererLaNav($slug);
-		// j'ai des donnees, je le affiche
-		include "view/page-display.php";
-	}
+    {
+        // definition d'un slug par defaut (en cas d'appel sans parametre dans l'url)
+        $slug = 'teletubbies';
+        // recuperation du slug du parametre d'url si present
+        if (isset($_GET['p'])) {
+            $slug = $_GET['p'];
+        }
+        // en PHP 7
+        // $slug = $_GET['p'] ?? $_POST['p'] ?? 'teletubbies';
+        // recuperation les donnees de la page qui correspond au slug
+        $page = $this->repository->getBySlug($slug);
+        // si il n'y a pas de donnees, erreur 404
+        if ($page === false) {
+            // 404
+            include "view/404.php";
 
-	private function genererLaNav($slug)
-	{
-		ob_start();
-		$data = $this->repository->findAll();
-		include "view/nav.php";
-		// generer la nav
-		$nav = ob_get_clean();
-		return $nav;
-	}
+            return;
+        }
+        // je dois avoir la nav initialisee pour que la vue la montre
+        $nav = $this->genererLaNav($slug);
+        // j'ai des donnees, je le affiche
+        include "view/page-display.php";
+    }
+
+    private function genererLaNav($slug)
+    {
+        ob_start();
+        $data = $this->repository->findAll();
+        include "view/nav.php";
+        // generer la nav
+        $nav = ob_get_clean();
+
+        return $nav;
+    }
 }
